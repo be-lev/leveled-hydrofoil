21411cb709a85e2506c35cd9501631d5c2ef2111
Oryam <48219965+or-yam@users.noreply.github.com>
2020-11-18 11:41:43 +0200

    added video background to home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import './Home.css';
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="video-background">
+        <div className="video-foreground">
+          <iframe
+            title="bgVideo"
+            src="https://www.youtube.com/embed/i9yCe6ibhzU?controls=0&showinfo=0&rel=0&autoplay=1&mute=1&loop=1&playlist=i9yCe6ibhzU"
+            allow="autoplay; encrypted-media"
+            frameBorder="0"
+            width="100vw"
+            height="100vh"
+            allowFullScreen
+          ></iframe>
+        </div>
+      </div>
+    </div>
+  );
 }
