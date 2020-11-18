4d73c28ac8fd477faa750797704993abfd366039
Oryam <48219965+or-yam@users.noreply.github.com>
2020-11-18 13:23:25 +0200

    veideo tag insted of iframe link

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,7 +4,46 @@ import './Home.css';
 export default function Home() {
   return (
     <div>
-      <div className="video-background">
+      <section className="showcase">
+        <div className="video-container">
+          <video
+            src="https://res.cloudinary.com/dnrxmm7a0/video/upload/v1605694748/levelfoil/LEVELED_Promo.mp4-_lmav7q.mp4"
+            autoPlay
+            muted
+            loop
+          ></video>
+        </div>
+        <div className="content">
+          <h1>Level-Hydrofoils</h1>
+          <h3>The future is here</h3>
+          <a href="#about" className="btn">
+            More Info
+          </a>
+        </div>
+      </section>
+
+      <section id="about">
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, qui.
+        </p>
+
+        <h2>Follow Us On Social Media</h2>
+
+        <div className="social">
+          <a href="#" target="">
+            <i className="fab fa-twitter fa-3x"></i>
+          </a>
+          <a href="#" target="">
+            <i className="fab fa-facebook fa-3x"></i>
+          </a>
+          <a href="#" target="">
+            <i className="fab fa-instagram fa-3x"></i>
+          </a>
+        </div>
+      </section>
+
+      {/* <div className="video-background">
         <div className="video-foreground">
           <iframe
             title="bgVideo"
@@ -16,7 +55,7 @@ export default function Home() {
             allowFullScreen
           ></iframe>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
